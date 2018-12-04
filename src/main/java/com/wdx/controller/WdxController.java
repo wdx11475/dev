3ca1ad4d9f378026f638c0380162b959da4f6f38
wangduanxiang <1147510415@qq.com>
2018-12-04 15:34:22 +0800

    git 版本回退one

--- src/main/java/com/wdx/controller/WdxController.java
+++ src/main/java/com/wdx/controller/WdxController.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class WdxController {
     public static void main(String[] args) {
-        System.out.println("git test history two ");
+        System.out.println("git test history one");
     }
 }
