a7726ea7908955e2051d478a459015905471e47d
wangduanxiang <1147510415@qq.com>
2018-12-04 15:38:32 +0800

    wdx

--- src/main/java/com/wdx/controller/WdxController.java
+++ src/main/java/com/wdx/controller/WdxController.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class WdxController {
     public static void main(String[] args) {
-        System.out.println("git test history one");
+        System.out.println("git test history one  wdx");
     }
 }
